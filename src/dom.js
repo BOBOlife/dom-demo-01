bd4bad56ce986fe9b82a0a3e2331c7cabe7e94d5
BOBOWang <bbw1996@qq.com>
2020-10-28 17:34:36 +0800

    update

--- src/dom.js
+++ src/dom.js
@@ -85,23 +85,6 @@ window.dom = {
       return node.innerHTML;
     }
   },
-  style(node, name, value) {
-    if (arguments.length === 3) {
-      // dom.style(div, 'color', 'red')
-      node.style[name] = value;
-    } else if (arguments.length === 2) {
-      if (typeof name === "string") {
-        // dom.style(div, 'color')
-        return node.style[name];
-      } else if (name instanceof Object) {
-        // dom.style(div, {color: 'red'})
-        const object = name;
-        for (let key in object) {
-          node.style[key] = object[key];
-        }
-      }
-    }
-  },
   class: {
     add(node, className) {
       node.classList.add(className);
